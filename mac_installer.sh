25341998e298115e3677e93851c372303c0bcd98
Afi <44731817+afi-dev@users.noreply.github.com>
2022-06-12 19:05:34 +0200

    Update mac_installer.sh

--- mac_installer.sh
+++ mac_installer.sh
@@ -44,7 +44,9 @@ function MIT() {
 
 Mit_Licensing=$(curl https://raw.githubusercontent.com/afi-dev/Termsteel/main/LICENSE >/dev/null)
 
-State_Mit_Licensing=$(osascript -e 2>&1 'display alert "LICENSE AGREEMENTS" buttons {"Reject","Agree"} message "'"${Mit_Licensing//\"/}"'"')
+State_Mit_Licensing=$(osascript 2>&1 <<End
+    display alert "LICENSE AGREEMENTS" message "'"${Mit_Licensing//\"/}"'" buttons {"Reject","Agree"}
+End
 
 if [[ "$State_Mit_Licensing" == 'Agree' ]]; then
     Eula
@@ -60,7 +62,9 @@ function Eula() {
 
 Eula_Agreements=$(curl https://raw.githubusercontent.com/afi-dev/Termsteel/main/EULA >/dev/null)
 
-State_Eula_Agreements=$(osascript -e 2>&1 'display alert "EULA AGREEMENTS" buttons {"Reject","Agree"} message "'"${Eula_Agreements//\"/}"'"')
+State_Eula_Agreements=$(osascript 2>&1 <<End
+    display alert "EULA AGREEMENTS" message "'"${Eula_Agreements//\"/}"'" buttons {"Reject","Agree"}
+End
 
 if [[ "$State_Eula_Agreements" == 'Agree' ]]; then
     Install
